f5d3fc1ec5ab8460f28ef9241899361b3fabb515
michal-zielonka <146939528+michal-zielonka@users.noreply.github.com>
2026-07-03 12:30:08 +0000

    chore: Model generation simplified, add precommit hooks
    Closes MPT-19816

--- frontend/src/api/ffc-api-model/models/AccountUpdate.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type AccountUpdate = {
-  name?: string | null;
-  integration?: "aws" | "google" | "microsoft" | "softwareone" | null;
-  external_id?: string | null;
-};
--- frontend/src/api/ffc-api-model/models/EntitlementStats.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type EntitlementStats = {
-  new?: number;
-  redeemed?: number;
-  terminated?: number;
-};
