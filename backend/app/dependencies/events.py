500b9adf7282bf6db63c41366a77446a2ebefd66
Francesco Faraone <ffaraone@gmail.com>
2026-09-09 13:02:12 +0200

    MPT-25066 fix use right client; fix meta condition for subscriptions
    (cherry picked from commit 25563428abe0b59736fa323e78e56bcca54a25cf)

--- backend/app/dependencies/events.py
+++ backend/app/dependencies/events.py
@@ -20,7 +20,7 @@ from app.events.subscriptions.processing import (
 
 def get_order_event_handler(
     api_modifier_client: APIModifierClient,
-    client: InstallationClient,
+    installation_client: InstallationClient,
     ext_client: ExtensionClient,
     optscale_auth_client: OptscaleAuthClient,
     optscale_client: OptscaleClient,
@@ -30,7 +30,7 @@ def get_order_event_handler(
 ) -> _OrderEventHandler:
     return _OrderEventHandler(
         api_modifier_client=api_modifier_client,
-        client=client,
+        installation_client=installation_client,
         ext_client=ext_client,
         optscale_auth_client=optscale_auth_client,
         optscale_client=optscale_client,
@@ -41,13 +41,13 @@ def get_order_event_handler(
 
 
 def get_subscription_event_handler(
-    client: InstallationClient,
+    installation_client: InstallationClient,
     ext_client: ExtensionClient,
     entitlement_repo: EntitlementRepository,
     auth_context: CurrentAuthContext,
 ) -> _SubscriptionEventHandler:
     return _SubscriptionEventHandler(
-        client=client,
+        installation_client=installation_client,
         ext_client=ext_client,
         entitlement_repo=entitlement_repo,
         account=auth_context.account,
